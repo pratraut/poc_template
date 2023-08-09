8066d4a165feba92d491d7bd3ecea6f6fa1ee7d8
Prathamesh Raut <pratham.raut07@gmail.com>
2023-08-09 14:25:28 +0530

    first commit

---
+++ test/PoC.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity 0.8.19;
+
+import {IERC20} from "@src/interfaces/IERC20.sol";
+
+import {PoC} from "@src/PoC.sol";
+import {IPoC} from "@src/interfaces/IPoC.sol";
+import {Test, console} from "forge-std/Test.sol";
+
+contract TestPoC is Test {
+    PoC public poc;
+    function setUp() public {
+        // Initialize and funds required addresses
+        poc = new PoC(address(0));
+        IPoC.TokenAmounts[] memory amounts = new IPoC.TokenAmounts[](0);
+
+        // Fund it
+        poc.fund(amounts);
+
+        // Initial snapshots of balances of tokens
+        address[] memory tokens = new address[](0);
+        poc.snapshotBalancePre(tokens);
+    }
+
+    function testPoC() public {
+        poc.attack();
+
+        // Print profit
+        poc.prettyPrintProfit();
+    }
+}
